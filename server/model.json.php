44180a4cdf2b5aae01ffd25f7788102f3a8a859c
Remy Lalanne <remy@damas-software.org>
2012-05-09 10:31:07 +0200

    JSON support continued

--- server/model.json.php
+++ server/model.json.php
@@ -1,17 +1,37 @@
 <?php
+/**
+ * JSON web service of DAMAS software (damas-software.org)
+ *
+ * Copyright 2005-2012 Remy Lalanne
+ *
+ * This file is part of damas-core.
+ *
+ * damas-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * damas-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with damas-core.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 session_start();
 
-header('Content-type: application/json');
-
-include_once "service.php"; //error_code()
+include_once "service.php";
 include_once "../php/data_model_1.json.php";
-
-$cmd = arg("cmd");
-$ret = false;
 
 damas_service::init_http();
 damas_service::accessGranted();
+damas_service::allowed( "model::" . arg("cmd") );
+
+$cmd = arg("cmd");
+$ret = false;
 
 if( !$cmd )
 {
@@ -19,216 +39,195 @@ if( !$cmd )
 	echo "Bad command";
 	exit;
 }
-if( !allowed( "model::" . $cmd ) )
-{
-	header("HTTP/1.1: 403 Forbidden"); //ERR_PERMISSION
-	echo "Permission denied";
-	exit;
-}
+
+header('Content-type: application/json');
 
 switch( $cmd )
 {
 	case "createNode":
-		if( is_null( arg('id') ) || is_null( arg('type') ) ){
+		if( is_null( arg('id') ) || is_null( arg('type') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$id = model::createNode( arg("id"), arg("type") );
-		if( !$id ) {
-			//$err = $ERR_NODE_CREATE;
-			header("HTTP/1.1: 409 Conflict");
+		if( !$id )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_CREATE;
 			echo "Error during the creation of node, please change your values";
 			exit;
 		}
-		if( $id )
-			$ret = model_json::node( $id, 1, $NODE_TAG | $NODE_PRM );
-		else
-			$ret = false;
+		$ret = model_json::node( $id, 1, $NODE_TAG | $NODE_PRM );
 		break;
 	case "duplicate":
-		if( is_null( arg('id') ) ){
+		if( is_null( arg('id') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$id = model::copyBranch( arg("id"), false );
-		if( !$id) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !$id )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_CREATE;
 			echo "Error during the copy of nodes, please change your values";
 			exit;
-			//$err = $ERR_NODE_CREATE;
 		}
-		if( $id )
-			$ret = model_json::node( $id, 1, $NODE_TAG | $NODE_PRM );
-		else
-			$ret = false;
+		$ret = model_json::node( $id, 1, $NODE_TAG | $NODE_PRM );
 		break;
 	case "removeNode":
-		if( is_null( arg('id') ) ){
+		if( is_null( arg('id') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::removeNode( arg("id") );
-		if( !$ret ) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::removeNode( arg("id") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_DELETE;
 			echo "Error during the deletion of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_DELETE;
 		}
 		break;
 	case "setKey":
-		if( is_null( arg('id') ) || is_null( arg('name') ) || is_null( arg('value') ) ){
+		if( is_null( arg('id') ) || is_null( arg('name') ) || is_null( arg('value') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::setKey( arg("id"), arg("name"), arg("value") );
-		if( !$ret ) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::setKey( arg("id"), arg("name"), arg("value") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "removeKey":
-		if( is_null( arg('id') ) || is_null( arg('name') ) ){
+		if( is_null( arg('id') ) || is_null( arg('name') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::removeKey( arg("id"), arg("name") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::removeKey( arg("id"), arg("name") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "move":
-		if( is_null( arg('id') ) || is_null( arg('target') ) ){
+		if( is_null( arg('id') ) || is_null( arg('target') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::move( arg("id"), arg("target") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::move( arg("id"), arg("target") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_MOVE;
 			echo "Error during the moving of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_MOVE;
 		}
 		break;
 	case "tag":
-		if( is_null( arg('id') ) || is_null( arg('name') ) ){
+		if( is_null( arg('id') ) || is_null( arg('name') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::tag( arg("id"), arg("name") );
-		if( !$ret ) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::tag( arg("id"), arg("name") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "untag":
-		if( is_null( arg('id') ) || is_null( arg('name') ) ){
+		if( is_null( arg('id') ) || is_null( arg('name') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::untag( arg("id"), arg("name") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::untag( arg("id"), arg("name") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "link":
-		if( arg('src') == null || arg('tgt') == null ) {
+		if( is_null( arg('src') ) || is_null( arg('tgt') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::link( arg("src"), arg("tgt") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::link( arg("src"), arg("tgt") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "unlink":
-		if( is_null( arg('id') ) ){
+		if( is_null( arg('id') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::unlink( arg("id") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::unlink( arg("id") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "setType":
-		if( is_null( arg('id') ) || is_null( arg('type') ) ){
+		if( is_null( arg('id') ) || is_null( arg('type') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::setType( arg("id"), arg("type") );
-		if( !$ret) {
-			header("HTTP/1.1: 409 Conflict");
+		if( ! model::setType( arg("id"), arg("type") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "setTags":
-		if( is_null( arg('id') ) || is_null( arg('tags') ) ){
+		if( is_null( arg('id') ) || is_null( arg('tags') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::setTags( arg("id"), arg("tags") );
-		if( !$ret ) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::setTags( arg("id"), arg("tags") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 	case "setKeys":
-		if( is_null( arg('id') ) || is_null( arg('old') ) || is_null( arg('new') ) ){
+		if( is_null( arg('id') ) || is_null( arg('old') ) || is_null( arg('new') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
-		$ret = model::setKeys( arg("id"), arg("old"), arg("new") );
-		if( !$ret ) {
-			header("HTTP/1.1: 409 Conflict");
+		if( !model::setKeys( arg("id"), arg("old"), arg("new") ) )
+		{
+			header("HTTP/1.1: 409 Conflict"); //$err = $ERR_NODE_UPDATE;
 			echo "Error during the updating of node, please change your values";
 			exit;
-			//$err = $ERR_NODE_UPDATE;
 		}
 		break;
 
@@ -240,104 +239,103 @@ switch( $cmd )
 	 */
 
 	case "ancestors":
-		if( is_null( arg('id') ) ){
+		if( is_null( arg('id') ) )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::multi( implode( ',', model::ancestors( arg('id') ) ), 1, $NODE_TAG | $NODE_PRM );
 		break;
 	case "searchKey": // should return array of ids (json array?) // this is done =D
-		if( is_null(arg('key')) || arg('value') == null ){
+		if( is_null(arg('key')) || arg('value') == null )
+		{
 			header("HTTP/1.1: 400 Bad Request"); //ERR_COMMAND
 			echo "Bad command";
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$array = model::searchKey( arg('key'), arg('value') );
 		$ret = model_json::multi( implode(',', $array), 1, $NODE_TAG | $NODE_PRM );
-		if( !$ret ) {
-			header('HTTP/1.1: 404 Not Found');
+		if( !$ret )
+		{
+			header('HTTP/1.1: 404 Not Found'); //$err = $ERR_NODE_ID;
 			exit;
-			//$err = $ERR_NODE_ID;
 		}
 		break;
 	case "single":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::node( arg( "id" ), 1, $NODE_TAG | $NODE_PRM );
-		if( !$ret ) {
-			header('HTTP/1.1: 404 Not Found');
+		if( !$ret )
+		{
+			header('HTTP/1.1: 404 Not Found'); //$err = $ERR_NODE_ID;
 			exit;
-			//$err = $ERR_NODE_ID;
 		}
 		break;
 	case "children":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::children( arg("id") );
 		break;
 	case "links":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::links( arg("id") );
 		break;
 	case "multi":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::multi( arg("id"), 1, $NODE_TAG | $NODE_PRM );
 		break;
 	case "graph":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::graph( arg("id") );
-		if (!$ret) {
-			header('HTTP/1.1: 404 Not Found');
+		if (!$ret)
+		{
+			header('HTTP/1.1: 404 Not Found'); //$err = $ERR_NODE_ID;
 			exit;
-			//$err = $ERR_NODE_ID;
 		}
 		break;
 	case "export":
-		if( is_null( arg('id') ) ){
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('id') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model_json::node( arg('id'), 0, $NODE_TAG | $NODE_PRM);
-		if( !$ret ) {
-			header('HTTP/1.1: 404 Not Found');
+		if( !$ret )
+		{
+			header('HTTP/1.1: 404 Not Found'); //$err = $ERR_NODE_ID;
 			exit;
-			//$err = $ERR_NODE_ID;
 		}
 		break;
 	case "search":
-		if( is_null( arg('value') ) ) {
-			header('HTTP/1.1: 400 Bad Request');
+		if( is_null( arg('value') ) )
+		{
+			header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND; break;
 			exit;
-			//$err = $ERR_COMMAND; break;
 		}
 		$ret = model::search( arg('value') );
 		break;
 	default:
-		header('HTTP/1.1: 400 Bad Request');
+		header('HTTP/1.1: 400 Bad Request'); //$err = $ERR_COMMAND;
 		exit;
-		//$err = $ERR_COMMAND;
 } // switch / case
 
 $nolog = array( 'single', 'children', 'multi', 'stats', 'types', 'tags', 'search' );
--- server/model.soap.php
+++ server/model.soap.php
@@ -1,12 +1,24 @@
 <?php
 /**
+ * SOAP web service of DAMAS software (damas-software.org)
  *
- * @requires service.php
+ * Copyright 2005-2012 Remy Lalanne
  *
+ * This file is part of damas-core.
  *
- * Author Remy Lalanne
+ * damas-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Copyright (c) 2005-2012 Remy Lalanne
+ * damas-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with damas-core.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 session_start();
 
