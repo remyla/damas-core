36096ccad897a891f6d88c105981be53383870f1
Rémy Lalanne <remy@damas-software.org>
2014-05-28 15:39:55 +0200

    code documentation and cleaning

--- php/asset.php
+++ php/asset.php
@@ -9,6 +9,13 @@
 
 class assets
 {
+	/**
+	 * Process the uploaded file
+	 * @param {Integer} $id the asset
+	 * @param {String} $path the path of the uploaded file in the temporary folder
+	 * @param {String} $message
+	 * @returns {Boolean} true on success, false otherwise
+	 */
 	static function asset_upload( $id, $path, $message )
 	{
 		switch( model::getKey( $id, 'mode' ) )
@@ -56,7 +63,8 @@ class assets
 	}
 
 	/**
-	 * Copy the current file version to backup folder, preserving mtime
+	 * Copy the current version to backup folder, preserving mtime
+	 * @param {Integer} $id the asset
 	 */
 	static function version_backup ( $id )
 	{
@@ -66,26 +74,15 @@ class assets
 		if( !copy( $opath, $bpath ) )
 			return false;
 		touch( $bpath, filemtime( $opath ) );
-		/*
-		$new_id = model::createNode( $id, "asset" );
-		if( !$new_id ) return false;
-		model::setKey( $new_id, "bytes", model::getKey( $id, 'bytes' ) );
-		model::setKey( $new_id, "file", assets::getbackuppath( $id ) );
-		model::setKey( $new_id, "sha1", model::getKey( $id, 'sha1' ) );
-		model::setKey( $new_id, "text", model::getKey( $id, 'text' ) );
-		model::setKey( $new_id, "time", model::getKey( $id, 'time' ) );
-		model::setKey( $new_id, "user", model::getKey( $id, 'user' ) );
-		model::setKey( $new_id, "version", model::getKey( $id, 'version' ) ? model::getKey( $id, 'version' ) : "0" );
-		*/
 		return model::create( "asset", array(
-				'#parent' => $id,
-				'bytes' => model::getKey( $id, 'bytes' ),
-				'file' => assets::getbackuppath( $id ),
-				'sha1' => model::getKey( $id, 'sha1' ),
-				'text' => model::getKey( $id, 'text' ),
-				'time' => model::getKey( $id, 'time' ),
-				'user' => model::getKey( $id, 'user' ),
-				'version' => model::getKey( $id, 'version' ) ? model::getKey( $id, 'version' ) : "0"
+			'#parent' => $id,
+			'bytes' => model::getKey( $id, 'bytes' ),
+			'file' => assets::getbackuppath( $id ),
+			'sha1' => model::getKey( $id, 'sha1' ),
+			'text' => model::getKey( $id, 'text' ),
+			'time' => model::getKey( $id, 'time' ),
+			'user' => model::getKey( $id, 'user' ),
+			'version' => model::getKey( $id, 'version' ) ? model::getKey( $id, 'version' ) : "1"
 		));
 	}
 
@@ -96,14 +93,12 @@ class assets
 	 */
 	static function version_increment( $id, $message )
 	{
-		if( !model::setKey( $id, "version", model::getKey( $id, 'version' ) + 1 ) )
-			return false;
-		if( !model::setKey( $id, "user", getUser() ) )
-			return false;
-		if( !model::setKey( $id, "text", $message ) )
-			return false;
-		if( !model::setKey( $id, "time", time() ) )
-			return false;
+		model::update($id, array(
+			"version" => model::getKey( $id, 'version' ) + 1,
+			"user" => getUser(),
+			"text" => $message,
+			"time" => time()
+		));
 		#global $assetsLCL;
 		#if( !model::setKey( $id, "sha1", sha1_file( $assetsLCL . model::getKey( $id, 'file' ) ) ) )
 		#	return false;
@@ -111,14 +106,26 @@ class assets
 	}
 
 	/**
-	 * @deprecated NOT USED (STUDIO100)
+	 * @deprecated (STUDIO100 versioning)
 	 * Increment the asset after a successful commit. The path is incremented too. Not backup needed 
 	 * @param {Integer} asset node index
 	 * @param {String} user message for the new version
 	 */
-/*
 	static function version_increment2 ( $id, $message )
 	{
+		$file =  model::getKey( $id, 'file' );
+		if( !$file ) return false;
+		return model::create( "asset", array(
+			'#parent' => $id,
+			//'bytes' => model::getKey( $id, 'bytes' ),
+			'file' => dirname(dirname($file)) . '/' . $version . '/' . basename($file),
+			//'sha1' => model::getKey( $id, 'sha1' ),
+			'text' => $message,
+			'time' => time(),
+			'user' => getUser(),
+			'version' => model::getKey( $id, "version" ) ? str_pad( model::getKey( $id, "version" ) + 1, 3, '0', STR_PAD_LEFT ) : "001"
+		));
+/*
 		$file =  model::getKey( $id, 'file' );
 		if( !$file ) return false;
 		$new_id = model::createNode( $id, "asset" );
@@ -133,8 +140,8 @@ class assets
 		model::setKey( $id, "time", model::getKey( $new_id, "time" ) );
 		model::setKey( $id, "version", model::getKey( $new_id, "version" ) );
 		return $new_id;
-	}
 */
+	}
 
 
 	//
@@ -142,6 +149,9 @@ class assets
 	//
 	//
 
+	/**
+	 *
+	 */
 	static function getbackupfolder( $id )
 	{
 		$path = model::getKey( $id, 'backupdir' );
@@ -153,6 +163,9 @@ class assets
 		return dirname( model::getKey( $id, 'file' ) );
 	}
 
+	/**
+	 *
+	 */
 	static function getbackupname( $id )
 	{
 		$opath = model::getKey( $id, 'file' );
@@ -162,6 +175,9 @@ class assets
 		return $filename . "-" . $version . "." . $pinfo['extension'];
 	}
 
+	/**
+	 *
+	 */
 	static function getbackuppath( $id )
 	{
 		return assets::getbackupfolder( $id ) . "/" . assets::getbackupname( $id );
