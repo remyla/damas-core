5e5ea640f8dce8087bb648ded4e72811dcafcb6f
Mathieu Valero <mathieu-valero@bell.net>
2015-04-14 12:52:02 +0200

    Update Remove keys

--- server-nodejs/model.js
+++ server-nodejs/model.js
@@ -20,6 +20,10 @@ db.open(function(err, db) {
   }
 });
 
+/**
+  * Creates a node providing its internal type value. Doesn't check parent node existence.
+  * @param {JSON Object} JSON Object containing the values of the fields to create for this node
+  */
 this.create= function(keys) {
   console.log('Add: ' + JSON.stringify(keys));
   db.collection('node', function(err, collection) {
@@ -33,56 +37,82 @@ this.create= function(keys) {
   });
 }
 
-function update(id, keys) {
-  console.log('Updating: ' + id);
+/**
+ * Remove a set of keys
+ * @param {Integer} $id node id
+ * @param {JSON Object} $name key name
+ */
+this.removeKey= function(id, keys){
   db.collection('node', function(err, collection) {
-    collection.findAndModify({'_id':new ObjectId(id)}, {$set: keys}, {safe:true}, function(err, result) {
+    collection.update({'_id':new ObjectId(id)}, {$unset: keys}, {safe:true}, function(err, result) {
+      if (err) {
+        console.log('Error updating: ' + err);
+      } else {
+        console.log(keys+ " removed");
+      }
+    });
+  });
+}
+
+
+/**
+ * Update the keys of a node. Specified keys overwrite existing keys, others are left untouched.
+ * A null key value removes the key.
+ * @param {Integer} $id node index
+ * @param {Array} $keys keys Array of key/value pairs to update (usually comming from json_decode)
+ */
+this.update= function(id, keys, res) {
+  console.log('Updating: ' + id);
+  console.log(keys);
+  var keyToRemove = {};
+  var self= this;
+  var keyNumber=0;
+  db.collection('node', function(err, collection) {
+    collection.update({'_id':new ObjectId(id)}, {$set:keys}, {safe:true}, function(err, result) {
       if (err) {
         console.log('Error updating: ' + err);
         res.send({'error':'An error has occurred'});
       } else {
         for(var k in keys)
-          if(keys[k]===null)
-            this.removeKey(id, k);
+          if(keys[k]===null){
+            keyToRemove[k]='';
+            ++keyNumber;
+          }
+          if(keyNumber>0)
+            self.removeKey(id, keyToRemove);
       }
+      res.send(id+" Updated")
     });
   });
 }
 
-function removeKey(id, key){
-  console.log('Removing: '+ key +' From '+ id);
-  var keyToRemove={};
-  keyToRemove[key]="";
+/**
+  * Recursively delete a node - WARNING: this function doesn't check anything before removal
+  * @return {Boolean} true on success, false otherwise
+  */
+this.deleteNode=function(id, res) {
+  console.log('Deleting: ' + new ObjectId(id));
   db.collection('node', function(err, collection) {
-    collection.update({'_id':new ObjectId(id)}, {$unset: keyToRemove}, {safe:true}, function(err, result) {
+    collection.remove({$or:[{'_id':new ObjectId(id)},{'tgt_id':id},{'src_id':id}]}, {safe:true}, function(err, result) {
       if (err) {
-        console.log('Error updating: ' + err);
-      } else {
-        console.log('Key '+ key +' removed from '+id);
-      }
-    });
-  });
-}
-
-function deleteNode(req, res) {
-  var id = req.params.id;
-  console.log('Deleting: ' + id);
-  db.collection('nodes', function(err, collection) {
-    collection.remove({$or:[{'_id':new ObjectId(id)},{'tgt_id':id},{'src_id':id},{'node_id':id}]}, {safe:true}, function(err, result) {
-      if (err) {
-        return false;
+        res.send("error");
       } else {
         console.log('' + result + ' document(s) deleted');
-        return true;
+        res.send(result+ " documents deleted");
       }
     });
   });
 }
 
-function read(id){
+/**
+ * Get key->values combinations for a given node
+ * @param {Integer} $id of the node
+ * @return {JSON Object} key=value pairs
+ */
+this.read= function(id,res){
   db.collection('node', function(err, collection) {
     collection.findOne({'_id':new ObjectId(id)},function(err, item) {
-      return item;
+      res.json(item);
     });
   });
 }
