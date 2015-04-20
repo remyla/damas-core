acefa2ec349f24669d1468b4b7dc626747977038
Mathieu Valero <mathieu-valero@bell.net>
2015-04-20 17:36:18 +0200

    NodeJS server

---
+++ server-nodejs/index.js
@@ -0,0 +1,24 @@
+
+var http     = require('http'),
+	https    = require('https'),
+	express  = require('express'),
+	app      = express(),
+	fs		 = require('fs'),
+	conf 	 = require('./conf.json'),
+
+	routes = require('./routes')(app);
+
+
+var confConn = conf.connection;
+
+//Options for https connection
+
+var options = {
+	key  : fs.readFileSync(confConn.pathKey + confConn.keyFile),
+	cert : fs.readFileSync(confConn.pathKey + confConn.cerFile)
+};
+
+//Creation server http & https
+var serverhttp  = http.createServer(app).listen(confConn.portHttp);
+
+    serverhttps = https.createServer(options, app).listen(confConn.portHttps);
