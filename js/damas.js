47321345a0549fa4b548f82fb60882db784857de
Rémy Lalanne <remy@damas-software.org>
2015-05-15 17:23:21 +0200

    commented URI encoding for now

--- js/damas.js
+++ js/damas.js
@@ -480,7 +480,8 @@
 	damas.get_rest = function ( query, callback )
 	{
 		var req = new XMLHttpRequest();
-		req.open('GET', this.server + encodeURIComponent(query), callback !== undefined);
+		req.open('GET', this.server + query, callback !== undefined);
+		//req.open('GET', this.server + encodeURIComponent(query), callback !== undefined);
 		req.onreadystatechange = function(e){
 			if(req.readyState == 4)
 			{
