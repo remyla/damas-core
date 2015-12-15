b201601a231907af608f671a57b60269e971f8d7
Remy Lalanne <remy@primcode.com>
2015-12-15 17:03:07 +0100

    separate upload handling

--- server-nodejs/routes/default.js
+++ server-nodejs/routes/default.js
@@ -14,82 +14,6 @@ module.exports = function(app, express){
 
 	mod.connection( function(){});
 
-	ncp.limit=16;
-	var checksum;
-	var tempFile;
-	var fileSystem=conf.fileSystem;
-	//Middlewares
-	app.use( bodyParser.json({limit: '50mb'}));
-	app.use( bodyParser.urlencoded( { limit: '50mb', extended : true } ) );
-
-	app.use( multer({
-		onError: function (error, next) {
-			console.log(error);
-			next(error);
-		},
-		onFileUploadStart: function (file) {
-			checksum = crypto.createHash('sha1');
-		},
-		onFileUploadData: function (file, data, req, res) {
-			checksum.update(data);
-		},
-		onFileUploadComplete: function (file, req, res) {
-			//var dest=decodeURIComponent(req.body.path);
-			var dest=fileSystem+decodeURIComponent(req.body.path).replace(/:/g,"");
-			dest=dest.replace(/\/+/,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
-				ncp(file.path, dest, function (err) {
-					if (err) {
-						return console.error(err);
-					}
-					else {
-						fs.unlinkSync(file.path);
-					}
-				});
-			});
-			var keys={};
-			keys.user=req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.file=decodeURIComponent(req.body.path);
-			keys.checksum = checksum.digest('hex');
-			keys.size = file.size;
-			console.log(req.body.id);
-			console.log(typeof(req.body.id));
-			if (req.body.id === 'null')
-			{
-				mod.create(keys, function(error, doc)
-				{
-					if (error)
-					{
-						res.status(409);
-						res.send('create Error, please change your values');
-					}
-					else
-					{
-						res.status(201);
-						res.send(doc);
-					}
-				});
-			}
-			else
-			{
-				mod.update(req.body.id, keys, function(error, doc)
-				{
-					if (error)
-					{
-						res.status(409);
-						res.send('update Error, please change your values');
-					}
-					else
-					{
-						res.status(201);
-						res.send(doc);
-					}
-				});
-			}
-		}
-	}));
-
 	app.use(methodOverride( function(req, res)
 	{
 		if ( req.body && typeof req.body === 'object' && '_method' in req.body )
@@ -514,90 +438,12 @@ db.things.find({$where: function() {
 		});
 	};
 
-	//Upload Management
-	upload=function (req, res){
-/*
-		if(done){
-			var dest=decodeURIComponent(req.body.path);
-			dest=fileSystem+dest.replace(/:/g,"");
-			dest=dest.replace(/\/+/,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
-				ncp(tempFile.path,dest, function (err) {
-					if (err) {
-						return console.error(err);
-					}
-					else {
-						fs.unlinkSync(tempFile.path);
-					}
-				});
-			});
-			var keys={};
-			keys.author=req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.file=decodeURIComponent(req.body.path);
-			keys.checksum=checksum;
-			mod.create(keys, function(error, doc)
-			{
-				if( error )
-				{
-					res.status(409);
-					res.send('create Error, please change your values');
-				}
-				else
-				{
-					res.status(201);
-					res.send(doc[0]);
-				}
-			});
-		}
-*/
-	};
-
-	uploadNewVersion=function (req, res){
-/*
-		if(done){
-			var dest=decodeURIComponent(req.body.path);
-			dest=fileSystem+dest.replace(/:/g,"");
-			dest=dest.replace(/\/+/g,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
-				ncp(tempFile.path,dest, function (err) {
-					if (err) {
-						return console.error(err);
-					}
-					else {
-						fs.unlinkSync(tempFile.path);
-					}
-				});
-			});
-			var keys={};
-			keys.author=req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.checksum=checksum;
-			mod.update(req.body.id,keys, function(error, doc)
-			{
-				if( error )
-				{
-					res.status(409);
-					res.send('Update Error, please change your values');
-				}
-				else
-				{
-					res.status(201);
-					res.send(doc);
-				}
-			});
-		}
-*/
-	};
-
 	//
 	// Extra operations
 	//
 	app.get('/api/graph/:id', graph);
 	app.get('/api/file/:path(*)',getFile);
 	app.post('/api/import', importJSON);
-	app.post('/api/upload', upload);
-	app.put('/api/upload', uploadNewVersion);
 	//app.get('/subdirs/:path',getSubdirs);
 	//app.get('/subdirs',getSubdirs);
 
