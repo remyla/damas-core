161863dab0c07d19c788be799867d510ba5514b0
gnoxr <quester@protonmail.com>
2016-04-15 12:46:05 +0200

    Added callback calls to allow answering requests with the debug DB

--- server-nodejs/lib/db/debug.js
+++ server-nodejs/lib/db/debug.js
@@ -5,40 +5,45 @@
 
 module.exports = function () {
     var self = this;
-    self.test = "testself";
-    var test = "testvar";
     self.debug = require('debug')('app:db:debug:' + process.pid);
 
     self.connect = function (conf, callback) {
         debug('Call to db.connect() with the following configuration:');
         debug(conf);
+        callback(true);
     };
 
     self.create = function (nodes, callback) {
         debug('Call to db.create() with the following nodes:');
         debug(nodes);
+        callback(true);
     };
 
     self.read = function (ids, callback) {
         debug('Call to db.read() with the following ids:');
         debug(ids);
+        callback(true);
     };
 
     self.update = function (ids, keys, callback) {
         debug('Call to db.update() with the following elements:');
         debug('Update ids: ', ids);
         debug('Update keys: ', keys);
+        callback(true);
     };
 
     self.remove = function (ids, callback) {
         debug('Call to db.delete() with the following ids:');
         debug(ids);
+        callback(true);
     };
 
     self.search = function (keys, callback) {
         debug('Call to db.search() with the following keys:');
         debug(keys);
+        callback(true);
     };
+
     return self;
 };
 
