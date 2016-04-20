c4905f906a71dea1dac02e92558b4c7cc83ee68e
gnoxr <quester@protonmail.com>
2016-04-20 15:25:23 +0200

    Updated the debug engine to match the other engine syntax

--- server-nodejs/lib/db/debug.js
+++ server-nodejs/lib/db/debug.js
@@ -3,42 +3,42 @@
  * Licensed under the GNU GPL v3
  */
 
-module.exports = function () {
-    var self = this;
-    self.debug = require('debug')('app:db:debug:' + process.pid);
+module.exports = function (conf) {
+    var debug = require('debug')('app:db:debug:' + process.pid);
+    this.conf = conf;
 
-    self.connect = function (conf, callback) {
-        debug('Call to db.connect() with the following configuration:');
-        debug(conf);
+    this.connect = function (callback) {
+        debug('Call to db.connect(). Current configuration:');
+        debug(this.conf);
         callback(true);
     };
 
-    self.create = function (nodes, callback) {
+    this.create = function (nodes, callback) {
         debug('Call to db.create() with the following nodes:');
         debug(nodes);
         callback(true);
     };
 
-    self.read = function (ids, callback) {
+    this.read = function (ids, callback) {
         debug('Call to db.read() with the following ids:');
         debug(ids);
         callback(true);
     };
 
-    self.update = function (ids, keys, callback) {
+    this.update = function (ids, keys, callback) {
         debug('Call to db.update() with the following elements:');
         debug('Update ids: ', ids);
         debug('Update keys: ', keys);
         callback(true);
     };
 
-    self.remove = function (ids, callback) {
+    this.remove = function (ids, callback) {
         debug('Call to db.delete() with the following ids:');
         debug(ids);
         callback(true);
     };
 
-    self.search = function (keys, callback) {
+    this.search = function (keys, callback) {
         debug('Call to db.search() with the following keys:');
         debug(keys);
         callback(true);
