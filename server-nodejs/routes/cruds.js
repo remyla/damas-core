520ba8344a8743b3c6164facb3db98ba428f6b36
gnoxr <quester@protonmail.com>
2016-04-28 15:37:53 +0200

    Commented routes/cruds.js with the possible HTTP status codes

--- server-nodejs/routes/cruds.js
+++ server-nodejs/routes/cruds.js
@@ -1,12 +1,17 @@
-module.exports = function(app, express) {
-    var db  = app.locals.db;
-    //methodOverride = require('method-override' ),
-    var fs  = require('fs');
-    var multer  = require('multer');
+/*
+ * cruds.js - from Damas-Core
+ * Licensed under the GNU GPL v3
+ */
+
+module.exports = function (app, express) {
+    var db = app.locals.db;
+    //methodOverride = require('method-override'),
+    var fs = require('fs');
+    var multer = require('multer');
 
 /*
-    app.use(methodOverride(function(req, res) {
-        if (req.body && typeof req.body === 'object' && '_method' in req.body ) {
+    app.use(methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
             // look in urlencoded POST bodies and delete it
             var method = req.body._method;
             delete req.body._method;
@@ -15,17 +20,34 @@ module.exports = function(app, express) {
     }));
 
     //Handle errors
-    app.use(function(err, req, res, next) {
-        if (err ) {
+    app.use(function (err, req, res, next) {
+        if (err) {
             console.log("An error has occurred: "+ err);
-        }
-        else {
+        } else {
             next();
         }
     });
 */
 
-    /* CRUD operations */
+    /*
+     * CRUD operations
+     */
+
+
+    /*
+     * create()
+     *
+     * Method: PUT
+     * URI: /api/lock/
+     *
+     * Insert new nodes
+     *
+     * HTTP status codes:
+     * - 200: OK (nodes created)
+     * - 400: Bad request (not formatted correctly)
+     * ? 403: Forbidden (the user does not have the right permissions)
+     * - 409: Conflict (some nodes already exist with these identifiers)
+     */
     create = function (req, res) {
         if (Object.keys(req.body).length === 0) {
             res.status(400).send('create error: the body of the request is empty');
@@ -39,7 +61,7 @@ module.exports = function(app, express) {
             nodes[n].author = req.user.username||req.connection.remoteAddress;
             nodes[n].time = Date.now();
         }
-        db.create(nodes, function(error, doc) {
+        db.create(nodes, function (error, doc) {
             if (error) {
                 res.status(409).send('Create error, ' +
                     'please change your values');
@@ -47,15 +69,29 @@ module.exports = function(app, express) {
             }
             res.status(201).send(doc);
         });
-    };
+    }; // create()
 
-    read = function(req,res ) {
+
+    /*
+     * read()
+     *
+     * Method: GET
+     * URI: /api/
+     *
+     * Retrieve the specified nodes
+     *
+     * HTTP status codes:
+     * - 200: OK (nodes retrieved)
+     * - 400: Bad request (not formatted correctly)
+     * - 404: Not Found (the nodes do not exist)
+     */
+    read = function (req, res) {
         var id = req.params.id || req.body.id;
         if (!id) {
             res.status(400).send('read error: the specified id is not valid');
             return;
         }
-        db.read(id.split(","), function(error, doc) {
+        db.read(id.split(","), function (error, doc) {
             if (error) {
                 res.status(409).send('read error, please change your values');
                 return;
@@ -67,9 +103,24 @@ module.exports = function(app, express) {
             }*/
             res.status(200).send(doc);
         });
-    };
+    }; // read()
 
-    update = function(req, res ) {
+
+    /*
+     * update()
+     *
+     * Method: PUT
+     * URI: /api/
+     *
+     * Update existing nodes
+     *
+     * HTTP status codes:
+     * - 200: OK (nodes updated)
+     * - 400: Bad request (not formatted correctly)
+     * ? 403: Forbidden (the user does not have the right permissions)
+     * - 404: Not Found (the nodes do not exist)
+     */
+    update = function (req, res) {
 /*
         if (!ObjectId.isValid(req.params.id)) {
             res.status(400).send('update error: the specified id is not valid');
@@ -80,88 +131,129 @@ module.exports = function(app, express) {
             return;
         }
 
-        db.update(req.params.id.split(","), req.body, function(error, doc) {
+        db.update(req.params.id.split(","), req.body, function (error, doc) {
             if (error) {
                 res.status(409).send('update error, please change your values');
                 return;
             }
             res.status(200).json(doc);
         });
-    };
+    }; // update()
 
-    deleteNode = function(req, res) {
+
+    /*
+     * deleteNode()
+     *
+     * Method: DELETE
+     * URI: /api/
+     *
+     * Delete nodes
+     *
+     * HTTP status codes:
+     * - 200: OK (nodes deleted (or not found))
+     * - 400: Bad request (not formatted correctly)
+     * ? 403: Forbidden (the user does not have the right permissions)
+     */
+    deleteNode = function (req, res) {
         /* this check should not be based on ObjectId - disabled
         if (!ObjectId.isValid(req.params.id)) {
             res.status(400).send('error: the specified id is not valid');
             return;
         }
         */
-        db.remove(req.params.id.split(","), function(error, doc) {
+        db.remove(req.params.id.split(","), function (error, doc) {
             if (error) {
                 res.status(409).send('delete error, please change your values');
                 return;
             }
             res.status(200).send(doc.result.n + " documents deleted.");
         });
-    };
+    }; // deleteNode()
 
-    graph = function(req,res) {
+
+    /*
+     * graph()
+     *
+     * Method: GET
+     * URI: /api/graph/
+     *
+     * Retrieve the graph of nodes by following the links targeting them
+     *
+     * HTTP status codes:
+     * - 200: OK (graph retrieved)
+     * - 400: Bad request (not formatted correctly)
+     * - 404: Not Found (the nodes do not exist)
+     */
+    graph = function (req, res) {
         var id = req.params.id || req.body.id;
-        if (!id || id=="undefined") {
+        if (!id || id == "undefined") {
             res.status(400).send('Bad command');
             return;
         }
         //obsolete
-        db.graph(id.split(","), function(error, nodes) {
+        db.graph(id.split(","), function (error, nodes) {
             if (error) {
                 res.status(409).send('graph error, please change your values');
                 return;
             }
             if (nodes) {
                 res.status(200).json(nodes);
-            }
-            else
+            } else {
                 res.status(404).send('Id not found');
+            }
         });
-    };
+    }; // graph()
 
-    search = function(req, res) {
+
+    /*
+     * search()
+     *
+     * Method: GET
+     * URI: /api/search/
+     *
+     * Search for nodes in the database
+     *
+     * HTTP status codes:
+     * - 200: OK (search successful, even without results)
+     * - 400: Bad request (not formatted correctly)
+     */
+    search = function (req, res) {
         var q = req.params.query || req.body.query;
-        if (!q || q=="undefined") {
+        if (!q || q == "undefined") {
             res.status(400);
             res.send('Bad command');
             return;
         }
-        q = q.replace(/\s+/g,' ').trim();
-        //q = q.replace('< ','<');
-        //q = q.replace('<= ','<=');
-        //q = q.replace('>= ','>=');
-        //q = q.replace('> ','>');
-        //q = q.replace(': ',':');
+        q = q.replace(/\s+/g, ' ').trim();
+        //q = q.replace('< ', '<');
+        //q = q.replace('<= ', '<=');
+        //q = q.replace('>= ', '>=');
+        //q = q.replace('> ', '>');
+        //q = q.replace(': ', ':');
         var terms = q.split(" ");
         var pair;
-        var result={};
+        var result = {};
         //var j;
         //var tempField;
-        for(var i=0; i< terms.length; i++) {
+        for (var i = 0; i< terms.length; i++) {
             if (terms[i].indexOf('<=') > 0) {
                 pair = terms[i].split('<=');
-                result[pair[0]] = { $lte: decodeURIComponent(pair[1]) };
+                result[pair[0]] = {$lte: decodeURIComponent(pair[1])};
                 continue;
             }
             if (terms[i].indexOf('<') > 0) {
                 pair = terms[i].split('<');
-                result[pair[0]] = { $lt: decodeURIComponent(pair[1]) };
+                result[pair[0]] = {$lt: decodeURIComponent(pair[1])};
                 continue;
             }
             if (terms[i].indexOf('>=') > 0) {
                 pair = terms[i].split('>=');
-                result[pair[0]] = { $gte: decodeURIComponent(pair[1]) };
+                result[pair[0]] = {$gte: decodeURIComponent(pair[1])};
                 continue;
             }
             if (terms[i].indexOf('>') > 0) {
                 pair = terms[i].split('>');
-                result[pair[0]] = { $gt: decodeURIComponent(pair[1]) };
+                result[pair[0]] = {$gt: decodeURIComponent(pair[1])};
                 continue;
             }
             if (terms[i].indexOf(':') > 0) {
@@ -170,28 +262,27 @@ module.exports = function(app, express) {
 
                 var flags = value.replace(/.*\/([gimy]*)$/, '$1');
                 var pattern = value.replace(new RegExp('^/(.*?)/'+flags+'$'), '$1');
-                if (flags!=value && pattern!=value) {
+                if (flags != value && pattern != value) {
                     var regex = new RegExp(pattern, flags);
                     result[pair[0]] = regex;
-                }
-                else {
+                } else {
                     result[pair[0]] = value;
                 }
 /*
-                for(j=1;j<pair.length-1;j++)
-                    result[tempField]+=decodeURIComponent(pair[j])+":";
-                if (pair[j]!='')
-                    result[tempField]+=decodeURIComponent(pair[j]);
+                for (j = 1;j<pair.length-1;j++)
+                    result[tempField] += decodeURIComponent(pair[j])+":";
+                if (pair[j] != '')
+                    result[tempField] += decodeURIComponent(pair[j]);
 */
                 continue;
             }
 /* implement full text search
-            result['$where'] = function() {
+            result['$where'] = function () {
                 for (var key in this) {
-                    if (this[key] )
+                    if (this[key])
                 }
             }
-db.things.find({$where: function() {
+db.things.find({$where: function () {
   for (var key in this) {
     if (this[key] === "bar") {
       return true;
@@ -201,26 +292,41 @@ db.things.find({$where: function() {
 }});
 */
 /*
-            if (i==0) {
+            if (i == 0) {
                 continue;
             }
-            if (result[tempField]!='')
-                result[tempField]+= " "+terms[i];
-            else
-                result[tempField]+=terms[i];
+            if (result[tempField] != '') {
+                result[tempField] += " "+terms[i];
+            } else {
+                result[tempField] += terms[i];
+            }
 */
         }
-        db.search(result, function(error, doc) {
+        db.search(result, function (error, doc) {
             if (error) {
                 res.status(409).send('Read Error, please change your values');
                 return;
             }
             res.status(200).send(doc);
         });
-    }
+    }; // search()
 
-    search_mongo = function(req, res) {
-        if(typeof db.mongo_search !== "function") {
+
+    /*
+     * search_mongo()
+     *
+     * Method: POST
+     * URI: /api/search_mongo/
+     *
+     * Perform a research specific to the MongoDB database engine
+     *
+     * HTTP status codes:
+     * - 200: OK (search successful, even without results)
+     * - 400: Bad request (not formatted correctly)
+     * - 501: Not Implemented (MongoDB not in use)
+     */
+    search_mongo = function (req, res) {
+        if (typeof db.mongo_search !== "function") {
             res.status(409).send('MongoDB not in use');
             return;
         }
@@ -231,8 +337,7 @@ db.things.find({$where: function() {
             sort =  data.sort;
             limit =  data.limit | 0;
             skip =  data.skip | 0;
-        }
-        else {
+        } else {
             query = req.body.query;
             sort = req.body.sort;
             limit = req.body.limit | 0;
@@ -246,7 +351,7 @@ db.things.find({$where: function() {
                 }
                 if ('string' === typeof obj[key]) {
                     if (obj[key].indexOf('REGEX_') === 0) {
-                        obj[key] = new RegExp(obj[key].replace('REGEX_',''));
+                        obj[key] = new RegExp(obj[key].replace('REGEX_', ''));
                     }
                 }
             }
@@ -259,45 +364,43 @@ db.things.find({$where: function() {
                 res.status(200).json(ids);
             }
         });
-    }
+    }; //search_mongo()
+
 
     /**
      * Import a JSON graph commit from our current Php Server
      *
      */
-    importJSON = function(req, res ) {
+    importJSON = function (req, res) {
         var json = JSON.parse(req.body.text);
-        json.nodes.forEach(function(node, i, nodes) {
+        json.nodes.forEach(function (node, i, nodes) {
             var keys = node.keys;
             keys.mysqlid = node.id;
-            db.search({mysqlid:keys.mysqlid}, function(err, res) {
+            db.search({mysqlid:keys.mysqlid}, function (err, res) {
                 if (err) {
                     console.log('ERROR');
                     return;
                 }
                 if (res.length === 0) {
-                    db.create(keys, function(err, n) {
+                    db.create(keys, function (err, n) {
                         if (err) console.log('ERROR create')
                     });
-                }
-                else {
+                } else {
                     console.log('found mysqlid:'+keys.mysqlid);
                 }
-                if (i===nodes.length -1) {  // we finished inserting nodes
-                    json.links.forEach(function(link) {
-                        console.log(link );
-                        db.search({mysqlid:link.src_id.toString()}, function(err, res1) {
+                if (i === nodes.length -1) {  // we finished inserting nodes
+                    json.links.forEach(function (link) {
+                        console.log(link);
+                        db.search({mysqlid:link.src_id.toString()}, function (err, res1) {
                             if (!err) {
-                                db.search({mysqlid:link.tgt_id.toString()}, function(err, res2) {
+                                db.search({mysqlid:link.tgt_id.toString()}, function (err, res2) {
                                     if (!err) {
-                                        db.create({src_id: res1[0], tgt_id: res2[0]}, function() {});
-                                    }
-                                    else {
+                                        db.create({src_id: res1[0], tgt_id: res2[0]}, function () {});
+                                    } else {
                                         console.log('LINK ERR');
                                     }
                                 });
-                            }
-                            else {
+                            } else {
                                 console.log('LINK ERR');
                             }
                         });
@@ -308,35 +411,55 @@ db.things.find({$where: function() {
         }, json);
         res.status(200);
         res.send();
-    };
+    }; // importJSON()
 
-    getFile= function(req,res) {
-        var path = fileSystem+decodeURIComponent(req.params.path).replace(/:/g,"").replace(/\/+/g,"/");
-        fs.exists(path, function(exists) {
+
+    /*
+     * getFile()
+     *
+     * Method: GET
+     * URI: /api/file/
+     *
+     * Get a file from the filesystem
+     *
+     * HTTP status codes:
+     * - 200: OK (file retrieved)
+     * - 400: Bad request (not formatted correctly)
+     * - 404: Not Found (the file does not exist)
+     */
+    getFile= function (req, res) {
+        var path = fileSystem+decodeURIComponent(req.params.path).replace(/:/g, "").replace(/\/+/g, "/");
+        fs.exists(path, function (exists) {
             if (exists) {
-                var stream = fs.createReadStream(path, { bufferSize: 64 * 1024 });
+                var stream = fs.createReadStream(path, {bufferSize: 64 * 1024});
                 res.writeHead(200);
                 stream.pipe(res);
-            }
-            else {
+            } else {
                 res.status(404);
                 res.send('File not found');
             }
         });
-    };
+    }; // getFile()
 
-    //
+
+    /*
+     * Register the operations
+     */
+
+    // CRUDS operations
+    app.post('/api/', create);
+    app.get('/api/:id', read);
+    app.put('/api/:id', update);
+    app.delete('/api/:id', deleteNode);
+
     // Extra operations
-    //
     app.get('/api/graph/:id', graph);
-    app.get('/api/file/:path(*)',getFile); // untested
+    app.get('/api/file/:path(*)', getFile); // untested
     app.post('/api/import', importJSON); // untested
-    //app.get('/subdirs/:path',getSubdirs);
-    //app.get('/subdirs',getSubdirs);
+    //app.get('/subdirs/:path', getSubdirs);
+    //app.get('/subdirs', getSubdirs);
 
-    //
-    // Alternative Operations ()
-    //
+    // Alternative Operations
     app.get('/api/search/:query(*)', search);
     app.post('/api/search_mongo', search_mongo);
     app.get('/api/graph/', graph);
@@ -344,14 +467,6 @@ db.things.find({$where: function() {
     app.get('/api/read/:id', read);
     //app.put('/', update);
     //app.delete('/', deleteNode);
-
-    //
-    // CRUDS operations
-    //
-    app.post('/api/', create);
-    app.get('/api/:id', read);
-    app.put('/api/:id', update);
-    app.delete('/api/:id', deleteNode);
 }
 
 
