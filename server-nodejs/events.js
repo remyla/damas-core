fbe0a47e79bf9d06628ef11774bce320d336fa70
gnoxr <quester@protonmail.com>
2016-05-02 17:07:31 +0200

    Listeners' context is an event controller

--- server-nodejs/events.js
+++ server-nodejs/events.js
@@ -4,10 +4,52 @@
  */
 
 var listeners = {};
+var manager = module.exports = new EventManager();
 
-module.exports = new EventLayer();
 
-function EventLayer() {
+/*
+ * EventContext()
+ * Constructor for a context object for event listeners
+ */
+function EventContext(name, id, data) {
+    this.next = id + 1;
+    this.data = data;
+    this.listener = listeners[name][id];
+
+    /*
+     * detach()
+     * Detach the current event listener
+     */
+    this.detach = function () {
+        manager.detach(name, listeners[name][id]);
+        --this.next;
+    };
+
+
+    /*
+     * attach()
+     * Attach another listener to the current event
+     */
+    this.attach = function (callback) {
+        manager.attach(name, callback);
+    };
+
+
+    /*
+     * interrupt()
+     * Interrupt the event (don't call other listeners)
+     */
+    this.interrupt = function () {
+        this.next = listeners[name].length;
+    };
+}
+
+
+/*
+ * EventManager()
+ * Set of methods to easily and agnostically manage events
+ */
+function EventManager() {
     var self = this;
 
     /*
@@ -15,16 +57,21 @@ function EventLayer() {
      * Fire an event: call all of its listeners
      */
     this.fire = function (hook) {
-        var args = [];
-        for (var i = 1; i < arguments.length; ++i) {
-            args.push(arguments[i]);
-        }
+        var data = {};
         if (hook in listeners) {
-            for (var id of Object.keys(listeners[hook])) {
-                // Call the listener using itself as a context
-                listeners[hook][id].apply(listeners[hook][id], args);
+            var args = [];
+            for (var i = 1; i < arguments.length; ++i) {
+                args.push(arguments[i]);
+            }
+            for (var l = 0; l < listeners[hook].length;) {
+                var context = new EventContext(hook, l, data);
+                listeners[hook][l].apply(context, args);
+
+                data = context.data;
+                l = context.next;
             }
         }
+        return data;
     }; // fire()
 
 
@@ -44,12 +91,11 @@ function EventLayer() {
      * attach()
      * Attach an event listener to an event
      */
-    this.attach = function (hook, id, callback) {
+    this.attach = function (hook, callback) {
         if (!(hook in listeners)) {
-            listeners[hook] = {};
+            listeners[hook] = [];
         }
-        // Note that we allow the override of existing listeners
-        listeners[hook][id] = callback;
+        listeners[hook].push(callback);
     }; // attach()
 
 
@@ -57,9 +103,12 @@ function EventLayer() {
      * detach()
      * Detach an event listener
      */
-    this.detach = function (hook, id) {
-        if (hook in listeners && id in listeners[hook]) {
-            delete listeners[hook][id];
+    this.detach = function (hook, callback) {
+        if (hook in listeners) {
+            var i = listeners[hook].indexOf(callback);
+            if (-1 !== i) {
+                listeners[hook].splice(i, 1);
+            }
         }
     }; // detach()
 }
