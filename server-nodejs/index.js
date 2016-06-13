c0501d04ab78b24ce053063da64bc06bb3896dd8
gnoxr <quester@protonmail.com>
2016-06-13 15:54:26 +0200

    Revert a73d2cf refactorization of index.js

--- server-nodejs/index.js
+++ server-nodejs/index.js
@@ -32,34 +32,33 @@ if (module.parent) {
 }
 
 debug('Working in %s mode', app.get('env'));
+var socket = require('./events/socket');
 
-function startServer(type, port) {
-    global.server = server;
-    server.listen(port, function () {
-        debug('%s server listening on port %s', type, port);
-        var socket = require('./events/socket');
-        socket.attach(server);
-    });
-}
+
+/*
+ * Create a HTTP server
+ */
+var http_port = process.env.HTTP_PORT || 8090;
+debug('Creating HTTP server on port %s', http_port);
+var http = require('http').createServer(app).listen(http_port, function () {
+    debug('HTTP server listening on port %s', http_port);
+    socket.attach(http);
+});
 
 /*
  * Create a HTTPS server if there are certificates
- * Create a HTTP server otherwise
  */
 if (conf.connection && conf.connection.Key && conf.connection.Cert) {
     var fs = require('fs');
-    var port = process.env.HTTPS_PORT || 8443;
-    debug('Creating HTTPS server on port %s', port);
-    global.server = require('https').createServer({
+    var https_port = process.env.HTTPS_PORT || 8443;
+    debug('Creating HTTPS server on port %s', https_port);
+    var https = require('https').createServer({
         key: fs.readFileSync(conf.connection.Key).toString(),
         cert: fs.readFileSync(conf.connection.Cert).toString()
-    }, app);
-    startServer('HTTPS', port);
-} else {
-    var port = process.env.HTTP_PORT || 8090;
-    debug('Creating HTTP server on port %s', port);
-    global.server = require('http').createServer(app);
-    startServer('HTTPS', port);
+    }, app).listen(https_port, function () {
+        debug('HTTPS server listening on port %s', https_port);
+        socket.attach(https);
+    });
 }
 
 
