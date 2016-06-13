e13d6b207c74334f155e26249c646df0b03aece5
gnoxr <quester@protonmail.com>
2016-06-13 16:56:44 +0200

    Fill the user object with a 'class' and an 'address'

--- server-nodejs/routes/auth-jwt-node.js
+++ server-nodejs/routes/auth-jwt-node.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     var conf = app.locals.conf;
 
     var expressJwt = require('express-jwt');
-    var jwt = require("jsonwebtoken");
+    var jwt = require('jsonwebtoken');
     var unless = require('express-unless');
     var crypto = require('crypto');
     var debug = require('debug')('app:routes:auth:' + process.pid);
@@ -29,7 +29,7 @@ module.exports = function (app) {
                 });
             });
         };
-        func.unless = require("express-unless");
+        func.unless = require('express-unless');
         return func;
     };
 
@@ -40,12 +40,12 @@ module.exports = function (app) {
     app.use('/api', jwtMiddleware.unless({path:['/api/signIn']}));
 
     var authenticate = function (req, res, next) {
-        debug("Processing authenticate middleware");
+        debug('Processing authenticate middleware');
         if (!req.body.username || !req.body.password) {
             debug('no username or password');
             return res.status(401).json('Invalid username or password');
         }
-        db.search({"username": req.body.username}, function (err, doc) {
+        db.search({'username': req.body.username}, function (err, doc) {
             if (err || doc.length === 0) {
                 return res.status(401).json('Invalid username or password');
             }
@@ -54,14 +54,16 @@ module.exports = function (app) {
                 if (crypto.createHash(conf.jwt.passwordHashAlgorithm).update(req.body.password).digest('hex') !== user.password) {
                     return res.status(401).json('Invalid username or password');
                 }
-                debug("User authenticated, generating token");
+                debug('User authenticated, generating token');
                 user.token = jwt.sign({ _id: user._id, username: user.username }, conf.jwt.secret, { expiresIn: conf.jwt.exp*60 });
                 var decoded = jwt.decode(user.token);
                 user.token_exp = decoded.exp;
                 user.token_iat = decoded.iat;
                 delete user.password;
-                debug("Token generated for user: %s, token: %s", user.username, user.token);
+                debug('Token generated for user: %s, token: %s', user.username, user.token);
                 req.user = user;
+                req.user.address = req.connection.remoteAddress;
+                req.user.class = req.user.class || 'guest';
                 return res.status(200).json(req.user);
             });
         });
@@ -87,16 +89,16 @@ module.exports = function (app) {
     app.use(function (err, req, res, next) {
         var errorType = typeof err,
             code = 500,
-            msg = { message: "Internal Server Error" };
+            msg = { message: 'Internal Server Error' };
 
         switch (err.name) {
-            case "UnauthorizedError":
+            case 'UnauthorizedError':
                 code = err.status;
                 msg = undefined;
                 break;
-            case "BadRequestError":
-            case "UnauthorizedAccessError":
-            case "NotFoundError":
+            case 'BadRequestError':
+            case 'UnauthorizedAccessError':
+            case 'NotFoundError':
                 code = err.status;
                 msg = err.inner;
                 break;
@@ -108,18 +110,18 @@ module.exports = function (app) {
     });
 */
 
-    app.get("/api/verify", function (req, res) {
-    	var token = fetch(req.headers);
-    	jwt.verify(token, conf.jwt.secret, function (err, decode) {
-    		if (err) {
-    			req.user = undefined;
-    			return res.status(401).send('invalid token');
-    		}
-    		return res.status(200).json(req.user);
-    	});
+    app.get('/api/verify', function (req, res) {
+        var token = fetch(req.headers);
+        jwt.verify(token, conf.jwt.secret, function (err, decode) {
+            if (err) {
+                req.user = undefined;
+                return res.status(401).send('invalid token');
+            }
+            return res.status(200).json(req.user);
+        });
     });
 
-    app.route("/api/signIn").post(authenticate, function (req, res, next) {
+    app.route('/api/signIn').post(authenticate, function (req, res, next) {
         return res.status(200).json(req.user);
     });
 }
