c82c74454355b89f662516a894875495e1d1985a
gnoxr <quester@protonmail.com>
2016-06-14 17:57:04 +0200

    Update the upload() route accordingly to the new DB specifications

--- server-nodejs/routes/upload.js
+++ server-nodejs/routes/upload.js
@@ -1,4 +1,8 @@
-module.exports = function(app){
+/*
+ * Licensed under the GNU GPL v3
+ */
+
+module.exports = function (app) {
 	var db = app.locals.db;
 	var conf = app.locals.conf;
 
@@ -8,12 +12,12 @@ module.exports = function(app){
 	var mkdirp = require('mkdirp');
 	var crypto = require('crypto');
 
-	ncp.limit=16;
+	ncp.limit= 16;
 	var checksum;
 	var tempFile;
-	var fileSystem=conf.fileSystem;
+	var fileSystem= conf.fileSystem;
 
-	app.use( multer({
+	app.use(multer({
 		onError: function (error, next) {
 			console.log(error);
 			next(error);
@@ -25,138 +29,48 @@ module.exports = function(app){
 			checksum.update(data);
 		},
 		onFileUploadComplete: function (file, req, res) {
-			var dest=fileSystem+decodeURIComponent(req.body.path).replace(/:/g,"");
-			dest=dest.replace(/\/+/,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
+			var dest = fileSystem + decodeURIComponent(req.body.path).replace(/:/g, '');
+			dest = dest.replace(/\/+/, '/');
+			mkdirp(dest.replace(/\/[^\/]*$/, ''), function (err) {
 				ncp(file.path, dest, function (err) {
 					if (err) {
 						return console.error(err);
-					}
-					else {
+					} else {
 						fs.unlinkSync(file.path);
 					}
 				});
 			});
-			var keys={};
-			keys.author = req.user.username || req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.file=decodeURIComponent(req.body.path);
-			keys.checksum = checksum.digest('hex');
-			keys.size = file.size;
-			console.log(req.body.id);
-			console.log(typeof(req.body.id));
-			if (req.body.id === 'null')
-			{
-				db.create(keys, function(error, doc)
-				{
-					if (error)
-					{
+			var node = {
+				author: req.user.username,
+				time: Date.now(),
+				file: decodeURIComponent(req.body.path),
+				checksum: checksum.digest('hex'),
+				size: file.size,
+			};
+			if (!req.body.id || 'null' === req.body.id) {
+				db.create([node], function (error, doc) {
+					if (error) {
 						res.status(409);
 						res.send('create Error, please change your values');
-					}
-					else
-					{
+					} else {
 						res.status(201);
-						res.send(doc[0]);
+						res.json(doc[0]);
 					}
 				});
-			}
-			else
-			{
-				db.update([req.body.id], keys, function(error, doc)
-				{
-					if (error)
-					{
+			} else {
+				node._id = req.body.id;
+				db.update([node], function (error, doc) {
+					if (error) {
 						res.status(409);
 						res.send('update Error, please change your values');
-					}
-					else
-					{
+					} else {
 						res.status(201);
-						res.send(doc);
+						res.json(doc[0]);
 					}
 				});
 			}
 		}
 	}));
-
-	//Upload Management
-	upload=function (req, res){
-	/*
-		if(done){
-			var dest=decodeURIComponent(req.body.path);
-			dest=fileSystem+dest.replace(/:/g,"");
-			dest=dest.replace(/\/+/,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
-				ncp(tempFile.path,dest, function (err) {
-					if (err) {
-						return console.error(err);
-					}
-					else {
-						fs.unlinkSync(tempFile.path);
-					}
-				});
-			});
-			var keys={};
-			keys.author=req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.file=decodeURIComponent(req.body.path);
-			keys.checksum=checksum;
-			db.create(keys, function(error, doc)
-			{
-				if( error )
-				{
-					res.status(409);
-					res.send('create Error, please change your values');
-				}
-				else
-				{
-					res.status(201);
-					res.send(doc[0]);
-				}
-			});
-		}
-	*/
-	};
-
-	uploadNewVersion=function (req, res){
-	/*
-		if(done){
-			var dest=decodeURIComponent(req.body.path);
-			dest=fileSystem+dest.replace(/:/g,"");
-			dest=dest.replace(/\/+/g,"/");
-			mkdirp(dest.replace(/\/[^\/]*$/,""),function(err){
-				ncp(tempFile.path,dest, function (err) {
-					if (err) {
-						return console.error(err);
-					}
-					else {
-						fs.unlinkSync(tempFile.path);
-					}
-				});
-			});
-			var keys={};
-			keys.author=req.connection.remoteAddress;
-			keys.time=Date.now();
-			keys.checksum=checksum;
-			db.update(req.body.id,keys, function(error, doc)
-			{
-				if( error )
-				{
-					res.status(409);
-					res.send('Update Error, please change your values');
-				}
-				else
-				{
-					res.status(201);
-					res.send(doc);
-				}
-			});
-		}
-	*/
-	};
-
-	app.post('/api/upload', upload);
-	app.put('/api/upload', uploadNewVersion);
-
 }
+
+
