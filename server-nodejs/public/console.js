069ed7b6286ffe4d0696c8dfc7fb2a302e40b2b3
Remy Lalanne <remy@primcode.com>
2016-07-15 18:05:06 +0200

    new console with require and upload and separate signIn

---
+++ server-nodejs/public/console.js
@@ -0,0 +1,115 @@
+require.config({
+	paths: {
+		'damas': "damas",
+		'utils': "utils",
+		'ui_log': "ui_log",
+		'ui_upload': 'generic-ui/scripts/uiComponents/ui_upload',
+		'domReady': '//raw.githubusercontent.com/requirejs/domReady/latest/domReady'
+	},
+	urlArgs: "v=" +  (new Date()).getTime()
+});
+
+require(['domReady', "damas", "utils"], function (domReady, damas) {
+	require(["ui_log","ui_upload"], function () {
+	window.damas = damas;
+	loadCss('console.css');
+	damas_connect('/api/', function (res) {
+		if (!res) {
+			window.location='/signIn?back=console'
+		}
+		if (res){
+			if (damas.user)
+			{
+				document.querySelector('.username').innerHTML = damas.user.username;
+				document.querySelector('#signOut').style.display = 'inline';
+				document.querySelector('#menubar1').style.display = 'block';
+			}
+			document.querySelector('#menubar2').style.display = 'block';
+			show_log();
+		}
+		else
+		{
+			document.querySelector('#connection').style.display = 'block';
+		}
+
+				var button = document.getElementById('but_locks');
+				button.addEventListener('click', function(e){
+					show_locks();
+				});
+				var button = document.getElementById('but_users');
+				button.addEventListener('click', function(e){
+					show_users();
+				});
+
+
+				/* UI */
+				var signOut = document.getElementById('signOut');
+				signOut.addEventListener('click', function( e ){
+					damas.signOut( function(e){
+						localStorage.removeItem("token");
+						localStorage.removeItem("user");
+						window.location='/signIn?back=console'
+						//document.location.reload();
+					});
+				});
+
+		/**
+		 * Methods
+		 */
+		function show_users(){
+			//damas.search('username:/.*/', function(res){
+			damas.search_mongo({'username':'REGEX_.*'}, {"username":1},0,0, function(res){
+				damas.read(res, function(users){
+					console.log(users);
+					window.users = users;
+					var out = document.querySelector('#contents');
+					out.innerHTML = '';
+					for(var i=0; i<users.length; i++)
+					{
+						out.innerHTML +=  '<li title="'+JSON_tooltip(users[i])+'">'+users[i].username+'</li>';
+					}
+				});
+			});
+		}
+
+		function show_locks(){
+			//damas.search('lock:/.*/', function(res){
+			damas.search_mongo({'lock':'REGEX_.*'}, {"lock":1},0,0, function(res){
+				damas.read(res, function(assets){
+					var out = document.querySelector('#contents');
+					var str = '<table><tr><th>lock &xdtri;</th><th>file</th></tr>'
+					for(var i=0; i<assets.length; i++)
+					{
+						str +=  '<tr title="'+JSON_tooltip(assets[i])+'"><td>'+assets[i].lock+'</td><td>'+assets[i].file+'</td></tr>';
+					}
+					str += '</table>';
+					out.innerHTML = str;
+				});
+			});
+		}
+
+		function show_wait(){
+				damas.search_mongo(
+					{online:"1", file:{$exists: true}, ino_write: {$exists: false}, time: { $gt: 1463200887000} },
+					{origin:1, file:1}, 0,0, function(res){
+
+				damas.read(res, function(assets){
+					var out = document.querySelector('#contents');
+					var str = '<table><tr><th>file</th><th>origin &xutri;</th></tr>';
+					for(var i=0; i<assets.length; i++)
+					{
+						str +=  '<tr>';
+						//str +=  '<td title="'+JSON_tooltip(assets[i])+'">'+assets[i].file+'</td>';
+						 str +=  '<td title="'+JSON_tooltip(assets[i])+'"><span class="nomobile">'+assets[i].file.split('/').slice(0,-1).join('/')+'/</span>'+assets[i].file.split('/').pop()+'</td>';
+						str +=  '<td>'+(assets[i].origin||'?')+'</td>';
+						str +=  '</tr>';
+					}
+					str += '</table>';
+					out.innerHTML = str;
+				});
+			});
+		}
+
+	});
+	});
+});
---
+++ server-nodejs/public/ui_log.js
@@ -0,0 +1,174 @@
+
+    require(['domReady'], function (domReady) {
+        domReady(function () {
+            //loadBtUpload(); 
+
+
+				var button = document.getElementById('but_log');
+				button.addEventListener('click', function(e){
+					show_log();
+				});
+
+
+        });
+    });
+
+		function show_log(){
+			offsetElements = 0;
+			compLog(document.querySelector('#contents'));
+			tableBody = document.querySelector('#contents tbody');
+		}
+
+	/**
+	* Generate Log Table
+	* 
+	*/
+	var ui_log = {
+	};
+		nbElements = 100;
+		offsetElements = 0;
+
+	compLog = function(container){
+		container.innerHTML = '';
+		var tableBody = tableLog(container);
+		damas.search_mongo({'time': {$exists:true}, '#parent':{$exists:true}}, {"time":-1},nbElements,offsetElements, function(res){
+			damas.read(res, function(assets){ 
+				tableLogContent(tableBody, assets);
+					offsetElements += nbElements;
+			});
+		});
+	};
+
+	var scrollElem = window;
+
+	scrollElem.addEventListener('scroll', function(){
+	//console.log(scrollElem.scrollY);
+		//if (scrollElem.scrollHeight - scrollElem.scrollTop === scrollElem.clientHeight){
+		if (scrollElem.scrollY === scrollElem.scrollMaxY){
+			damas.search_mongo({'time': {$exists:true}, '#parent':{$exists:true}}, {"time":-1},nbElements,offsetElements, function(res){
+				damas.read(res, function(assets){
+					tableLogContent(tableBody, assets);
+					offsetElements += nbElements;
+				});
+			});
+		}
+	});
+
+
+
+function tableLog(container) {
+	var table = document.createElement('table');
+	var thead = document.createElement('thead');
+	var th1 = document.createElement('th');
+	var th2 = document.createElement('th');
+	var th3 = document.createElement('th');
+	var tbody = document.createElement('tbody');
+
+	th1.innerHTML = 'time &xutri;';
+	th2.innerHTML = 'file';
+	th3.innerHTML = 'comment';
+
+	th1.style.width = '15ex';
+	
+	thead.appendChild(th1);
+	thead.appendChild(th2);
+	thead.appendChild(th3);
+	table.appendChild(thead);
+	table.appendChild(tbody);
+
+	container.appendChild(table);
+	return tbody;
+}
+
+/**
+* Generate Table Content
+* 
+*/
+function tableLogContent(container, assets) {
+	for (var i=0; i<assets.length; i++) {
+		container.appendChild(tableLogTr(assets[i]));
+	}
+}
+
+function tableLogTr(asset) {
+	var tr = document.createElement('tr');
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+	var td3 = document.createElement('td');
+	//td2.className = 'clickable';
+	var time = new Date(parseInt(asset.time));
+	var file = asset.file || asset['#parent'];
+	td1.setAttribute('title', time);
+	td1.style.width = '15ex';
+	td1.innerHTML = ('00'+time.getDate()).slice(-2)+'/'+('00'+time.getMonth()).slice(-2)+' '+('00'+time.getHours()).slice(-2)+':'+('00'+time.getMinutes()).slice(-2)+':'+('00'+time.getSeconds()).slice(-2);
+	td2.setAttribute('title', JSON_tooltip(asset));
+	td2.innerHTML = '<span class="nomobile">'+file.split('/').slice(0,-1).join('/')+'/</span>'+file.split('/').pop();
+	//td3.style.whiteSpace = 'normal';
+	td3.innerHTML = '&lt;'+asset.author+'&gt; '+asset.comment;
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tr.appendChild(td3);
+	return tr;
+}
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) { // AMD
+        define(['socket.io/socket.io'], factory);
+    } else if (typeof module === 'object' && module.exports) { // Node
+        module.exports = factory(require('socket.io-client'));
+    } else { // Browser globals
+        root.returnExports = factory(root.io);
+    }
+}(this, function (io) {
+    if (typeof window !== 'undefined') {
+        var address = location.protocol + '//' + location.host;
+        var socket = io.connect(address, { path: '/socket.io' });
+
+        window.addEventListener('beforeunload', function (event) {
+            socket.close();
+        });
+    } else {
+        // Suppose a local Socket.io server over TLS
+        var address = 'wss://0.0.0.0:8443';
+        var socket = io.connect(address, {
+            path: '/socket.io',
+            rejectUnauthorized: false
+        });
+    }
+
+    socket.on('connect', function () {
+        console.log('Connected to the Socket server ' + address);
+    });
+
+    socket.on('disconnect', function (reason) {
+        console.log('Disconnected: ' + reason);
+    });
+
+    socket.on('create', function (nodes) {
+        console.log(nodes.length + ' nodes created');
+        console.log(nodes);
+        var tbody = document.querySelector('tbody');
+        nodes.forEach(function(node){
+            if (node.time !== undefined && node['#parent'] !== undefined ) {
+                var tr = tableLogTr(node);
+                tr.style.opacity = '0';
+                tbody.insertBefore(tr, tbody.firstChild);
+                setTimeout(function() {
+                    tr.style.opacity = '1';
+                }, 1);
+            }
+        });
+    });
+
+    socket.on('update', function (nodes) {
+        console.log(nodes.length + ' nodes updated');
+        console.log(nodes);
+    });
+
+    socket.on('remove', function (nodes) {
+        console.log(nodes.length + ' nodes removed');
+        console.log(nodes);
+    });
+
+    return socket;
+}));
