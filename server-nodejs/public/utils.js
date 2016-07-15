073b889a008ab604531a288d37e5f635110649f7
Remy Lalanne <remy@primcode.com>
2016-07-15 17:59:06 +0200

    added damas.user on login and loadCss function

--- server-nodejs/public/utils.js
+++ server-nodejs/public/utils.js
@@ -1,3 +1,13 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define([], factory);
+	} else {
+		// Browser globals
+		root.ui_upload = factory();
+	}
+}(this, function () {
+
 /**
  * Get a multiline text description of the element
  * @return {string} the text string of the element compound of its label, tags
@@ -11,13 +21,13 @@ JSON_tooltip = function ( obj )
         text += " (" + typeof obj[keys[i]] + ") " + keys[i] + "  : " + obj[keys[i]];
         if (i !== keys.length - 1) {
             text += '\n';
-       }
+        }
     }
     return text;
 }
 
 /**
- * Take care of the connection, on top of damas.js. The token is store in the
+ * Take care of the connection, on top of damas.js. The token is stored in the
  * localStorage.
  * @param {string} server_url
  * @param {function} callback 
@@ -28,6 +38,20 @@ damas_connect = function (server_url, callback)
     damas.server = server_url;
     if (localStorage) {                                                                                                              
         damas.token = localStorage.getItem("token");
+	damas.user = JSON.parse(localStorage.getItem("user"));
     }
     damas.verify(callback);
 }
+
+function loadCss(url) {
+	var link = document.createElement("link");
+	link.type = "text/css";
+	link.rel = "stylesheet";
+	link.href = url;
+	document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+window.loadCss = loadCss;
+
+
+}));
