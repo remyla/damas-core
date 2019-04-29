3a61c23521eb33298066cde25b37f379e3fe5f08
axl-pis <axelpisanipro@gmail.com>
2019-04-29 17:59:34 +0200

    Added extension last_activity

--- server-nodejs/extensions/last_activity.js
+++ server-nodejs/extensions/last_activity.js
@@ -7,17 +7,14 @@
  * for current user in req.user.lastActivity
  */
 module.exports = function (app) {
-	app.use(function (req, res, next) {
-		if(req.connection.remoteAddress === req.user.username){
-			console.warn('no user connected');
-			next();
-		} else if (undefined != req.user) {					
-			let lastActivity = Date.now();
-			let usr = {'_id': req.user._id, 'lastActivity': lastActivity};
-			let db = app.locals.db;
-			db.update([usr], function(err, nodes){
-			});
-			next();
-		};
-	});
+    app.use(function (req, res, next) {
+        if ((undefined =! req.user) && req.user._id) {
+            let lastActivity = Date.now();
+            let usr = {'_id': req.user._id, 'lastActivity': lastActivity};
+            app.locals.db.update([usr], function (err, nodes) {
+            });
+        };
+        next();
+    });
 };
+
