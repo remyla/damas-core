e1a0dbef7e6c13cb72faf46bfa6e860872770961
Remy Lalanne <remy@primcode.com>
2020-11-17 10:45:16 +0100

    cli show errors on stderr close #254

--- server-nodejs/extensions/jwt_delegate.js
+++ server-nodejs/extensions/jwt_delegate.js
@@ -2,40 +2,45 @@
  * jwt_delegate.js
  *
  * Make a request to the server in file conf.json and stock the user node in the tracker.
+ *
+ *
  */
  
 module.exports = function (app) {
-    let db  = app.locals.db;
-    let XHR = require('xmlhttprequest');
+    var db  = app.locals.db;
+    //let XHR = require('xmlhttprequest-ssl');
+    var XMLHttpRequest = require("xmlhttprequest-ssl").XMLHttpRequest;
+    var debug = require('debug')('damas:extensions:jwt_delegate');
 
-    server = app.locals.conf.jwt_delegate.server;
+
+    var server = app.locals.conf.jwt_delegate.server;
     
     app.use('/api/signIn', function (req, res, next) {
+	    debug(server);
         if ('undefined' != req.body.username) {
-            let login = "username=" + req.body.username + "&password=" + req.body.password;
-            let request = new XHR.XMLHttpRequest();
-            request.open("POST", server, true);
-            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            request.onerror = function () {
+            var xhr = new XMLHttpRequest();
+            xhr.onerror = function () {
+	    debug(server);
                 console.error("Connection impossible with " + "'" + server + "'");
             }
-            request.onload = function (err) {
-                if (405 === request.status) {
-                    request.onerror();   
+            //xhr.onload = function (err) {
+            xhr.onreadystatechange = function (err) {
+                if (405 === this.status) {
+                    this.onerror();   
                 }
-                if (4 === request.readyState && 200 === request.status) { 
-                    responseText = JSON.parse(request.responseText);
-                    if (undefined === responseText.user) {
+                if (4 === this.readyState && 200 === this.status) { 
+                    var response = JSON.parse(this.responseText);
+                    if (undefined === response.user) {
                         console.warn("Impossible authentication with that version of damas.core. Please, update damas-core or disabled jwt_delegate extention.");
                         return next();
                     }
-                    db.create([responseText.user], function (err, nodes) {
+                    db.create([response.user], function (err, nodes) {
                         if (err) {
                             console.warn("Creation user node impossible in the database.");
                             return next();
                         }
                         if (null === nodes[0]) {
-                            db.update([responseText.user], function (err, doc) {
+                            db.update([response.user], function (err, doc) {
                                 if (err) {
                                     console.warn("Updating user node impossible.");
                                     return next();
@@ -45,7 +50,11 @@ module.exports = function (app) {
                     });
                 }
             }
-            request.send(login);
+            xhr.open("POST", server, true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            var login = "username=" + req.body.username + "&password=" + req.body.password;
+	    debug(login);
+            xhr.send(login);
         }
         next();
     });
