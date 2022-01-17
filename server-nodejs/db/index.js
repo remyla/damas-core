b4c5c983af9f77abf42e1913cb16b13ea804fb57
Remy Lalanne <remy@primcode.com>
2022-01-17 17:45:43 +0100

    restored missing db files

---
+++ server-nodejs/db/index.js
@@ -0,0 +1,40 @@
+/*
+ * database.js - from Damas-Core
+ * Licensed under the GNU GPL v3
+ */
+
+module.exports = function (type, settings) {
+    var debug = require('debug')('app:db:' + process.pid);
+    debug('Specified database : ' + type);
+    var db = null;
+
+    /*
+     * Choose the database to instantiate
+     */
+    switch (type) {
+    case 'mongodb':
+        var dbClass = require('./mongodb');
+        db = new dbClass(settings);
+        break;
+    case 'mongodb-fileids':
+        var dbClass = require('./mongodb-fileids');
+        db = dbClass(settings);
+        break;
+    default:
+        var dbClass = require('./debug');
+        db = new dbClass(settings);
+    }
+
+    /*
+     * Initialize the database object
+     */
+    db.connect(function (err) {
+        if (err) {
+            debug('Error: could not connect to the database.');
+        }
+    });
+
+    return db;
+}
+
+
